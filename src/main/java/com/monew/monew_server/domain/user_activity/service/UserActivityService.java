43faee80732a1551e6e9286e2c463664f8ebe478
backKim1024 <dydgml7313@naver.com>
2025-11-03 02:57:46 +0900

    feat(User) : 사용자 댓글을 조회하는 서비스 구현

--- src/main/java/com/monew/monew_server/domain/user_activity/service/UserActivityService.java
+++ src/main/java/com/monew/monew_server/domain/user_activity/service/UserActivityService.java
@@ -1,13 +1,18 @@
 package com.monew.monew_server.domain.user_activity.service;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 import com.monew.monew_server.domain.user.entity.User;
 import com.monew.monew_server.domain.user.repository.UserRepository;
+import com.monew.monew_server.domain.user_activity.dto.UserActivityDto;
 import com.monew.monew_server.domain.user_activity.dto.UserInfoDto;
 import com.monew.monew_server.domain.user_activity.mapper.UserActivityMapper;
+import com.monew.monew_server.domain.user_activity.repository.UserActivityCommentRepository;
+import com.monew.monew_server.domain.user_activity.repository.UserActivityQueryRepository;
+import com.monew.monew_server.domain.user_activity.repository.UserActivitySubscriptionRepository;
 import com.monew.monew_server.exception.ErrorCode;
 import com.monew.monew_server.exception.NotFoundException;
 
@@ -19,6 +24,9 @@ public class UserActivityService {
 
 	private final UserRepository userRepository;
 	private final UserActivityMapper userActivityMapper;
+	private final UserActivitySubscriptionRepository userActivitySubscriptionRepository;
+	private final UserActivityQueryRepository userActivityQueryRepository;
+	private final UserActivityCommentRepository userActivityCommentRepository;
 
 	// 사용자 기본 정보 조회 (닉네임, 이메일)
 	public UserInfoDto getUserInfo(UUID userId) {
@@ -28,4 +36,36 @@ public class UserActivityService {
 
 		return userActivityMapper.toUserInfoDto(user);
 	}
+
+	// 사용자가 구독중인 관심사 조회
+	public UserActivityDto getUserActivity(UUID userId) {
+		// 사용자 정보 가져오기
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND, "User not found with id: " + userId));
+
+		// 구독 중인 관심사 10개 조회
+		List<String> subscriptions = userActivitySubscriptionRepository
+			.findTop10ByUserIdOrderByCreatedAtDesc(userId)
+			.stream()
+			.map(s -> s.getInterest().getName())
+			.toList();
+
+		// 최근 작성한 댓글
+		List<String> comments = userActivityCommentRepository
+			.findTop10ByUser_IdOrderByCreatedAtDesc(userId)
+			.stream()
+			.map(c -> c.getContent())
+			.toList();
+
+
+		return userActivityMapper.toDto(
+			user,
+			subscriptions,
+			comments,
+			List.of(),
+			List.of()
+			);
+	}
+
+
 }

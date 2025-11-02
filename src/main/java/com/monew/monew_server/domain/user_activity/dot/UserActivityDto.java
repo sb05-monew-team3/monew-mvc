b915dcfd7ee410a02402ca1a0ea1da470ff26e68
backKim1024 <dydgml7313@naver.com>
2025-11-03 00:29:09 +0900

    test(User) :  사용자 활동내역 조회를 위한 UserActivityDto 구현

---
+++ src/main/java/com/monew/monew_server/domain/user_activity/dot/UserActivityDto.java
@@ -0,0 +1,30 @@
+package com.monew.monew_server.domain.user_activity.dot;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class UserActivityDto {
+
+	private UUID id;
+	private String email;
+	private String nickname;
+	private OffsetDateTime createdAt;
+
+	private List<String> subscriptions;
+	private List<String> comments;
+	private List<String> commentLikes;
+	private List<String> articleViews;
+
+
+}

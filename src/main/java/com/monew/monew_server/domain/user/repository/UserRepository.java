d52ae150bd0dd584263b30f04b824c4da2ef9825
backKim1024 <dydgml7313@naver.com>
2025-11-03 02:30:53 +0900

    refactor(User) :  사용자 정보 조회를 위해 UserRepository 수정

--- src/main/java/com/monew/monew_server/domain/user/repository/UserRepository.java
+++ src/main/java/com/monew/monew_server/domain/user/repository/UserRepository.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.monew.monew_server.domain.user.entity.User;
@@ -16,5 +18,13 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
 	// 로그인
 	Optional<User> findByEmail(String email);
+
+	// 사용자 정보 조회
+	@Query("""
+		SELECT u
+		FROM User u
+		WHERE u.id = :userId
+		""")
+	Optional<User> findUserInfoById(@Param("userId") UUID userId);
 
 }

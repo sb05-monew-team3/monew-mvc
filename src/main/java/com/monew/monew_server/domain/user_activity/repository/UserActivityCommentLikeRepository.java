e6c78c7fc7744d0f1f6a0c032934f6eaa5c5da43
backKim1024 <dydgml7313@naver.com>
2025-11-03 03:00:42 +0900

    feat(User) : 특정 사용자가 좋아요한 댓글을 조회하는 repository 구현

---
+++ src/main/java/com/monew/monew_server/domain/user_activity/repository/UserActivityCommentLikeRepository.java
@@ -0,0 +1,13 @@
+package com.monew.monew_server.domain.user_activity.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.monew.monew_server.domain.comment.entity.CommentLike;
+
+public interface UserActivityCommentLikeRepository extends JpaRepository<CommentLike, UUID> {
+	// 특정 사용자가 좋아요한 댓글 10
+	List<CommentLike> findTop10ByUser_IdOrderByCreatedAtDesc(UUID userId);
+}

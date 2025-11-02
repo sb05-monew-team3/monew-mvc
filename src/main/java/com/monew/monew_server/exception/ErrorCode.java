8475ffee4cb7185981f20720e5a85ce9bc73e40f
backKim1024 <dydgml7313@naver.com>
2025-11-03 03:21:13 +0900

    refactor(User) : USER_NOT_FOUND enum 추가

--- src/main/java/com/monew/monew_server/exception/ErrorCode.java
+++ src/main/java/com/monew/monew_server/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
 	INVALID_REQUEST("잘못된 요청입니다."),
 	ARTICLE_NOT_FOUND("요청하신 게시글을 찾을 수 없습니다."),
 	INTEREST_NOT_FOUND("관심사를 찾을 수 없습니다."),
-	INTEREST_NAME_DUPLICATION("유사한 이름의 관심사가 이미 존재합니다.");
+	INTEREST_NAME_DUPLICATION("유사한 이름의 관심사가 이미 존재합니다."),
+	USER_NOT_FOUND("요청하신 사용자를 찾을 수 없습니다.");
 
 	private final String message;
 }

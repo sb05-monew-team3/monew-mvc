5c7d90b3427f0ed72a2dca0a6a9ca54cd88d492b
backKim1024 <dydgml7313@naver.com>
2025-11-03 16:54:10 +0900

    refactor(User) : UserActivityCommentLikeRepository.java 리펙토링

--- src/main/java/com/monew/monew_server/domain/user_activity/repository/UserActivityCommentLikeRepository.java
+++ src/main/java/com/monew/monew_server/domain/user_activity/repository/UserActivityCommentLikeRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.monew.monew_server.domain.comment.entity.CommentLike;
 
+@Repository
 public interface UserActivityCommentLikeRepository extends JpaRepository<CommentLike, UUID> {
 	// 특정 사용자가 좋아요한 댓글 10
 	List<CommentLike> findTop10ByUser_IdOrderByCreatedAtDesc(UUID userId);

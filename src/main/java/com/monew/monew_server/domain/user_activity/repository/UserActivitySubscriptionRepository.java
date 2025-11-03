a3b40e3b056c168c104117516903d093592bfcaa
backKim1024 <dydgml7313@naver.com>
2025-11-03 16:55:09 +0900

    refactor(User) : UserActivitySubscriptionRepository.java 리펙토링

--- src/main/java/com/monew/monew_server/domain/user_activity/repository/UserActivitySubscriptionRepository.java
+++ src/main/java/com/monew/monew_server/domain/user_activity/repository/UserActivitySubscriptionRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.monew.monew_server.domain.interest.entity.Subscription;
 
+@Repository
 public interface UserActivitySubscriptionRepository extends JpaRepository<Subscription, UUID> {
 	// 사용자별 구독 중인 관심사
 	List<Subscription> findTop10ByUserIdOrderByCreatedAtDesc(UUID userId);

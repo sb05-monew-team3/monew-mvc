573cf751e55f2647fb63436ad57bbdeeb59e6c92
BakJiSeok <commicat2@gmail.com>
2025-11-05 23:31:40 +0900

    fix: start 스크립트 수정

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,7 +5,10 @@ APP_DIR="/home/ubuntu/app/monew-server"
 CONTAINER_NAME="monew-server"
 IMAGE_URI_FILE="$APP_DIR/image_uri.txt"
 
-AWS_REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+AWS_REGION=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+
 ECR_HOST=$(aws ecr get-authorization-token --region $AWS_REGION --output text --query 'authorizationData[0].proxyEndpoint')
 
 echo "ECR Host: $ECR_HOST"

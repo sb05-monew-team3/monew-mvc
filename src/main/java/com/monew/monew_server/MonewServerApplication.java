6585751819308391c39516d0a6d256fd0c09f146
jeonggiju <halogiju123@gmail.com>
2025-11-05 11:24:26 +0900

    chore: main 메서드 println 포트번호 수정

--- src/main/java/com/monew/monew_server/MonewServerApplication.java
+++ src/main/java/com/monew/monew_server/MonewServerApplication.java
@@ -9,6 +9,6 @@ public class MonewServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MonewServerApplication.class, args);
 		System.out.println("home : http://localhost:8080/");
-		System.out.println("actuator : http://localhost:8080/actuator");
+		System.out.println("actuator : http://localhost:8081/actuator");
 	}
 }

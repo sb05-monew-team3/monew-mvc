508e0411451f9041f12f6d45531761a0651f671e
BakJiSeok <commicat2@gmail.com>
2025-11-05 21:35:41 +0900

    ci

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,11 +2,24 @@
 
 APP_DIR="/home/ubuntu/app/monew-server"
 CONTAINER_NAME="monew-server"
+IMAGE_URI_FILE="$APP_DIR/image_uri.txt"
 
-aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_REPOSITORY_URI}
+AWS_REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+ECR_HOST=$(aws ecr get-authorization-token --region $AWS_REGION --output text --query 'authorizationData[0].proxyEndpoint')
+
+echo "ECR Host: $ECR_HOST"
+echo "AWS Region: $AWS_REGION"
+
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_HOST
+
+if [ ! -f "$IMAGE_URI_FILE" ]; then
+  echo "이미지 URI 파일($IMAGE_URI_FILE)을 찾을 수 없습니다."
+  exit 1
+fi
+ECR_IMAGE_URI=$(cat $IMAGE_URI_FILE)
 
 if [ -z "$ECR_IMAGE_URI" ]; then
-  echo "ECR_IMAGE_URI 환경 변수가 설정되지 않았습니다."
+  echo "ECR_IMAGE_URI를 파일에서 읽어오지 못했습니다."
   exit 1
 fi
 
@@ -19,7 +32,7 @@ if [ $(docker ps -a -q -f name=^/${CONTAINER_NAME}$) ]; then
     docker rm ${CONTAINER_NAME}
 fi
 
-docker run -d --pull always \
+docker run -d \
   --name ${CONTAINER_NAME} \
   -p 8080:8080 \
   -e "SPRING_PROFILES_ACTIVE=prod" \
